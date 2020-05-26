15067a5b0d44d4a57a1f17cbfdfa8fc6eeb03f32
Navaladi Vk <navaladivk@outlook.com>
2020-05-26 11:38:27 +1100

    Created sol file

---
+++ SOL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SOL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
