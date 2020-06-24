6006404e6e80ca8517fd41b6b26408673f54d74b
Navaladi Vk <navaladivk@outlook.com>
2020-06-25 08:38:20 +1100

    a

--- SOL/Arrays.cs
+++ SOL/Arrays.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace SOL
 {
@@ -15,15 +17,34 @@ namespace SOL
 
 
 
-            // Below are the datastructures in c# to get push ,pop,insert and delete with ease will mention all the available things about that data structure below
+        // Below are the datastructures in c# to get push ,pop,insert and delete with ease will mention all the available things about that data structure below
 
-            //List<String>
-            //Queue<String>
-            //Stack<String>
+        //List<String>
 
+
+
+
+
+        //Stack<String>
+
+
+
+        //The Queue is a special case collection which represents a first in first out concept
+        //Queue<String>
+        //Queue qt = new Queue()
+        //Queue.dequeue() removes first element
+        //qt.Enqueue(); will add element at the last
+        // For each loop will start in in the order the insertion occured
+        public void Queue()
+        {
+            Queue<string> qt = new Queue<string>();
+            qt.Enqueue("1"); 
+            qt.Enqueue("2"); // will add element at last
+            qt.Dequeue(); // will remove first added element
+        }
 
         //4*4 = 16 bytes
-    string[] arra = new string[10];
+        string[] arra = new string[10];
 
 
         /// <summary>
@@ -43,11 +64,9 @@ namespace SOL
 
         public void Pop()
         {
-
-         
+            //  for pop we can use stack
             // below in the demo code for javascript
             //arra.push("e");
-
             //below code is the close thing to pop in c# with list
             List<string> ilstarray = arra.ToList();
           //  ilstarray.
--- SOL/Program.cs
+++ SOL/Program.cs
@@ -6,7 +6,8 @@ namespace SOL
     {
         static void Main(string[] args)
         {
-
+            Arrays array = new Arrays();
+            array.Queue();
             //  arrays 1st 6 min
             Console.WriteLine("Hello World!");
         }
